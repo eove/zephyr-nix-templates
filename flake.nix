a8e4c0a188349ed37d335eee36bbeca80819a0da
Julien Peeters <julien@mountainhacks.org>
2025-01-09 16:52:43 +0100

    feat: add devshell

--- flake.nix
+++ flake.nix
@@ -1,13 +1,28 @@
 {
   description = "Nix templates for Zephyr development";
 
-  outputs = { self }:
-  {
-    templates = {
-      quick-start = {
-        path = ./quick-start;
-        description = "A basic quick start";
-      };
-    };
+  inputs = {
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
+
+  outputs = { self, flake-utils, nixpkgs-unstable }:
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs-unstable { inherit system; };
+    in
+    {
+      templates = {
+        quick-start = {
+          path = ./quick-start;
+          description = "A basic quick start";
+        };
+      };
+
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [
+          statix
+        ];
+      };
+    });
 }
